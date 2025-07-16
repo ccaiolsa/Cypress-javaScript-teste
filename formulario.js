03d260a91d3d78b142fac9d950deff24697a3a9a
Caio Lucas <ccaiolsa@gmail.com>
2025-07-16 11:03:18 -0400

    Ultima alteração JavaScript

--- formulario.js
+++ formulario.js
@@ -1,4 +1,17 @@
 
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+
+function verificarCampos(){
+    if (nome.value.trim() && email.value.trim()){
+        botao.disabled = false;
+    }else{
+        botao.disabled = true;
+    }
+}
+
+nome.addEventListener('input', verificarCampos);
+email.addEventListener('input', verificarCampos);
 
 const botao = document.querySelector('#submit-btn');
 botao.addEventListener('click', function(event){
@@ -9,6 +22,7 @@ botao.addEventListener('click', function(event){
     const email = document.querySelector('#email').value;
     const mensagem = document.querySelector('#mensagem').value;
     const resposta = document.querySelector('#responseMessage');
+    
 
     form.style.display = 'none';
     resposta.classList.remove('oculto');
